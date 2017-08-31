c6b7c37c2c351d7778f0d6839c882e3253d2c719
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2017-08-31 18:22:18 +0200

    Use real values for pupil position and glints

--- math.py
+++ math.py
@@ -23,59 +23,80 @@ f_y = 3.37462371e+03         # in px
 pixel_pitch = 0.0025         # 2.5 micro meter pixel size in mm
 
 # position of nodal point of camera
-o = np.array((nodal_x, nodal_y, focal_length))
+nodal_point = np.array((0, 0, 0))
 
 # position of light source 1, 2
-ls = np.array(((-80, -15, 0), (80, -15, 0)))
-
-# position of glint image on sensor plane 1, 2
-gi = np.array(((-0.664, -1.03725, 0), (-0.6795, -1.03775, 0)))
-
-# position of pupil image on sensor plane 1, 2
-pi = np.array((-0.68275, -1.0585, 0))
-
-b = np.cross(np.cross(gi[0] - o, ls[0] - o), np.cross(ls[1] - o, gi[1] - o))
-b_norm = b / np.linalg.norm(b)
+source1 = np.array((-80, -15, -6))
+source2 = np.array((80, -15, -6))
 
 # transform img to camera coordinates
-def to_ccs((x, y, z), x_center, y_center, p_pitch, lmb):
-    return ((p_pitch * (x - x_center), p_pitch * (y - y_center), -lmb * z))
+def to_ccs(pt, x_center, y_center, p_pitch, lmb):
+    out = []
+    for row in pt:
+        out.append([p_pitch * (row[0] - x_center), p_pitch * (row[1] - y_center), -lmb])
+    return np.array(out)
 
-# for glints 1, 2
-def k_r(k_c, i):
-    num = k_c * np.dot(o - gi[i], b_norm) \
-        - np.sqrt(k_c**2 * np.dot((o - gi[i])**2, b_norm)**2 \
-                  - np.linalg.norm(o - gi[i])**2 * (k_c**2 - R**2))
-    denom = np.linalg.norm(o - gi[i])**2
+def b_norm(gi1, gi2, ls1, ls2, o):
+    b = np.cross(np.cross(gi1 - o, ls1 - o), \
+                 np.cross(ls2 - o, gi2 - o))
+    return (b / np.linalg.norm(b))
+
+# for glints 1, 2, pupil center ep = K | R, pt = gi | pi
+def k_rp(k_c, pt, bnorm, o, ep):
+    num = k_c * np.dot(o - pt, bnorm) \
+        - np.sqrt(k_c**2 * np.dot((o - pt)**2, bnorm)**2 \
+                  - np.linalg.norm(o - pt)**2 * (k_c**2 - ep**2))
+    denom = np.linalg.norm(o - pt)**2
     return (num / denom)
 
-# for pupil center
-def k_p(k_c):
-    num = k_c * np.dot(o - pi, b_norm) \
-        - np.sqrt(k_c**2 * np.dot((o - pi)**2, b_norm)**2 \
-                  - np.linalg.norm(o - pi)**2 * (k_c**2 - K**2))
-    denom = np.linalg.norm(o - pi)**2
-    return (num / denom)
+def r_i(k_c, pt, bnorm, o, ep):
+    return (o + k_rp(k_c, pt, bnorm, o, ep) * (o - pt))
 
-def r_i(k_c, i):
-    return (o + k_r(k_c, i) * (o - gi[i]))
+def c(k_c, bnorm, o):
+    return (o + k_c * bnorm)
 
-def c(k_c):
-    return (o + k_c * b_norm)
-
-def p(k_c):
-    return (o + k_p(k_c) * (o - pi))
+def p(k_c, pt, bnorm, o, ep):
+    return (o + k_rp(k_c, pt, bnorm, o, ep) * (o - pt))
 
 # minimize for k_c
-def minimize_this(k_c, i):
-    lhs = np.dot(ls[i] - r_i(k_c, i), r_i(k_c, i) - c(k_c)) * np.linalg.norm(o - r_i(k_c, i))
-    rhs = np.dot(o - r_i(k_c, i), r_i(k_c, i) - c(k_c)) * np.linalg.norm(ls[i] - r_i(k_c, i))
+def minimize_this(k_c, pt, bnorm, o, ep, ls):
+    lhs = np.dot(ls - r_i(k_c, pt, bnorm, o, ep), r_i(k_c, pt, bnorm, o, ep) \
+                 - c(k_c, bnorm, o)) * np.linalg.norm(o - r_i(k_c, pt, bnorm, o, ep))
+    rhs = np.dot(o - r_i(k_c, pt, bnorm, o, ep), r_i(k_c, pt, bnorm, o, ep) \
+                 - c(k_c, bnorm, o)) * np.linalg.norm(ls - r_i(k_c, pt, bnorm, o, ep))
     return (lhs - rhs)
 
+ppos = np.loadtxt('./data/pupilpos_lefteye.txt')
+rpos = np.loadtxt('./data/reflexpos_lefteye.txt')
+glint1 = rpos[:, [0, 1]]
+glint2 = rpos[:, [2, 3]]
+ppos_ccs = to_ccs(ppos, c_x, c_y, pixel_pitch, focal_length)
+glint1_ccs = to_ccs(glint1, c_x, c_y, pixel_pitch, focal_length)
+glint2_ccs = to_ccs(glint2, c_x, c_y, pixel_pitch, focal_length)
+
+def _main():
+    for i in range(len(ppos_ccs)):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(*ppos_ccs[i].T, c='r')
+        ax.scatter(*glint1_ccs[i].T, c='g')
+        ax.scatter(*glint2_ccs[i].T, c='b')
+        plt.grid()
+        plt.tight_layout()
+        ax.auto_scale_xyz([-0.2, -0.1], [-0.8, -0.6], [-7, -9])
+        plt.savefig('./imgs/frame_{:03d}.png'.format(i))
+        plt.close()
+
 def main():
+    bnorm = b_norm(glint1_ccs[0], glint2_ccs[0], source1, source2, nodal_point)
+
     # obtain k_c for both glints
-    kc1 = optimize.minimize(lambda x: minimize_this(x, 0), 0, tol=1e-4)
-    kc2 = optimize.minimize(lambda x: minimize_this(x, 1), kc1.x[0], tol=1e-4)
+    kc1 = optimize.minimize(minimize_this, 0, \
+                            args=(glint1_ccs[0], bnorm, nodal_point, R, source1), \
+                            tol=1e-4)
+    kc2 = optimize.minimize(minimize_this, kc1.x[0], \
+                            args=(glint2_ccs[0], bnorm, nodal_point, R, source1), \
+                            tol=1e-4)
     print("kc1:\n", kc1, "\nkc2:\n", kc2)
 
     # use mean as result
@@ -83,8 +104,8 @@ def main():
     print("\nk_c:\n", k_c)
 
     # calculate c and p from kc
-    c_res = c(k_c)
-    p_res = p(k_c)
+    c_res = c(k_c, bnorm, nodal_point)
+    p_res = p(k_c, ppos_ccs[0], bnorm, nodal_point, K)
     print("\nc_res:\n", c_res, "\np_res:\n", p_res)
 
     # optic axis is vector given by c & p
@@ -102,33 +123,32 @@ def main():
     k_g = c_res[2] / (np.cos(phi_eye + beta_eye) * np.cos(theta_eye + alpha_eye))
     print("\nk_g:\n", k_g)
 
-    # # plots
-    # xmin = -500
-    # xmax = 500
-    # x = range(xmin, xmax)
-    # rng = np.linspace(xmin, xmax, xmax - xmin)
-    # y1 = np.array([minimize_this(x, 0) for x in rng])
-    # y2 = np.array([minimize_this(x, 1) for x in rng])
-    # plt.plot(x, y1)
-    # plt.plot(x, y2)
-    # plt.grid()
-    # plt.tight_layout()
-    # plt.show()
+    # plots
+    xmin = -500
+    xmax = 500
+    x = range(xmin, xmax)
+    rng = np.linspace(xmin, xmax, xmax - xmin)
+    y1 = np.array([minimize_this(x, glint1_ccs[0], bnorm, nodal_point, R, source1) for x in rng])
+    y2 = np.array([minimize_this(x, glint2_ccs[0], bnorm, nodal_point, R, source1) for x in rng])
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.scatter((ls[0, 0], ls[1, 0]), (ls[0, 1], ls[1, 1]), (ls[0, 2], ls[1, 2]), c='r')
-    # ax.scatter((gi[0, 0], gi[1, 0]), (gi[0, 1], gi[1, 1]), (gi[0, 2], gi[1, 2]), c='k')
-    # ax.scatter(o[0], o[1], o[2], c='b')
-    # ax.scatter(pi[0], pi[1], pi[2], c='k')
-    # ax.scatter(c_res[0], c_res[1], c_res[2], c='g')
-    # ax.scatter(p_res[0], p_res[1], p_res[2], c='g')
-    # ax.plot([p_res[0], c_res[0]], [p_res[1], c_res[1]], [p_res[2], c_res[2]], c='g')
-    # plt.grid()
-    # plt.tight_layout()
-    # box = 40
-    # ax.auto_scale_xyz([-box, box], [-box, box], [-box, box])
-    # plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(*np.array((source1, source2)).T, c='r')
+    ax.scatter(*np.array((glint1_ccs[0], glint2_ccs[0])).T, c='g')
+    ax.scatter(*np.array(nodal_point).T, c='k')
+    ax.scatter(*ppos_ccs[0].T, c='w')
+    ax.scatter(*np.array((c_res, p_res)).T, c='g')
+    ax.plot(*np.array((c_res, p_res)).T, c='g')
+    plt.grid()
+    plt.tight_layout()
+    box = 80
+    ax.auto_scale_xyz([-box, box], [-box, box], [-box, box])
+    plt.show()
 
 if __name__ == '__main__':
     main()
