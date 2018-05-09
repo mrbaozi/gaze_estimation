85d4751b6738a4a03bacb0c023f7d714ba633958
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2018-05-09 15:46:22 +0200

    Add preprocessing module for direct loading of eyeosb json recordings

---
+++ pyosb/preprocess/preprocess.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+
+class Preprocessor(object):
+    def __init__(self, dataframe, args):
+        self.cam_x = args.cam_x
+        self.cam_y = args.cam_y
+        self.screen_x = args.screen_x
+        self.screen_y = args.screen_y
+        self.dataFrame = dataframe
+
+    def rescale_to_screen(self, key, screen_x=None, screen_y=None):
+        if screen_x is None:
+            screen_x = self.screen_x
+        if screen_y is None:
+            screen_y = self.screen_y
+        self.dataFrame[key + '.x'] *= screen_x
+        self.dataFrame[key + '.y'] *= screen_y
+
+    def rescale_to_cam(self, key, cam_x=None, cam_y=None):
+        if cam_x is None:
+            cam_x = self.cam_x
+        if cam_y is None:
+            cam_y = self.cam_y
+        self.dataFrame[key + '.x'] *= cam_x
+        self.dataFrame[key + '.y'] *= cam_y
+
+    def fix_reflex_positions(self):
+        for eye in ['left_eye', 'right_eye']:
+            for xy in ['x', 'y']:
+                eyecoord = self.dataFrame[eye + '.eyecoords.' + xy]
+                self.dataFrame[eye + '.reflex_center.' + xy] += eyecoord
+                self.dataFrame[eye + '.reflexpos.left.' + xy] += eyecoord
+                self.dataFrame[eye + '.reflexpos.right.' + xy] += eyecoord
+
+    def fliplr(self):
+        for eye in ['left_eye', 'right_eye']:
+            self.dataFrame[eye + '.pupilpos.x'] = self.cam_x - self.dataFrame[eye + '.pupilpos.x']
+            self.dataFrame[eye + '.reflex_center.x'] = self.cam_x - self.dataFrame[eye + '.reflex_center.x']
+            self.dataFrame[eye + '.reflexpos.left.x'] = self.cam_x - self.dataFrame[eye + '.reflexpos.left.x']
+            self.dataFrame[eye + '.reflexpos.right.x'] = self.cam_x - self.dataFrame[eye + '.reflexpos.right.x']
+
+    def flipud(self):
+        for eye in ['left_eye', 'right_eye']:
+            self.dataFrame[eye + '.pupilpos.y'] = self.cam_y - self.dataFrame[eye + '.pupilpos.y']
+            self.dataFrame[eye + '.reflex_center.y'] = self.cam_y - self.dataFrame[eye + '.reflex_center.y']
+            self.dataFrame[eye + '.reflexpos.left.y'] = self.cam_y - self.dataFrame[eye + '.reflexpos.left.y']
+            self.dataFrame[eye + '.reflexpos.right.y'] = self.cam_y - self.dataFrame[eye + '.reflexpos.right.y']
+
+    def get_dataframe(self):
+        return self.dataFrame
