8a6c9c7040bb1a0f7606b8974566e33a24a4316b
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2018-05-30 10:51:07 +0200

    Code style cleanup

--- pyosb/gaze/mapper.py
+++ pyosb/gaze/mapper.py
@@ -8,7 +8,6 @@ Equation references (x.xx) in functions correspond to above thesis.
 """
 
 
-import sys
 import numpy as np
 import numpy.linalg as LA
 from scipy.optimize import minimize
@@ -37,7 +36,8 @@ class GazeMapper(object):
 
     def solve_kc_phd1(self, kc, l, u, b, o, r):
         ou_n = (o - u) / LA.norm(o - u)
-        kq = kc * np.dot(ou_n, b) - np.sqrt(kc**2 * np.dot(ou_n, b)**2 - kc**2 + r**2)
+        kq = (kc * np.dot(ou_n, b)
+              - np.sqrt(kc**2 * np.dot(ou_n, b)**2 - kc**2 + r**2))
         q = o + kq * ou_n
         oq_n = (o - q) / LA.norm(o - q)
         lq_n = (l - q) / LA.norm(l - q)
@@ -47,7 +47,6 @@ class GazeMapper(object):
         c1 = curvaturecenter_c(kq[0], l1, u1, b, o, r)
         c2 = curvaturecenter_c(kq[1], l2, u2, b, o, r)
         return LA.norm(c1 - c2)**2
-
 
 
 ####
@@ -63,13 +62,13 @@ n1 = 1.3375
 n2 = 1.0
 
 # camera parameters (from calibration)
-focal_length = 1. / (1. / 8.42800210895 - 1. / 500.) # in mm
-nodal_x = 2.6996358692163716 # in mm
-nodal_y = 2.2439755534153347 # in mm
+focal_length = 1. / (1. / 8.42800210895 - 1. / 500.)  # in mm
+nodal_x = 2.6996358692163716                          # in mm
+nodal_y = 2.2439755534153347                          # in mm
 c_center = np.array([1.07985435e+03, 8.97590221e+02])
-f_x = 3.37120084e+03         # in px
-f_y = 3.37462371e+03         # in px
-p_pitch = 0.0025         # 2.5 micro meter pixel size in mm
+f_x = 3.37120084e+03                                  # in px
+f_y = 3.37462371e+03                                  # in px
+p_pitch = 0.0025                                      # 2.5 micro meter
 phi_cam = -17.0
 theta_cam = 0.0
 kappa_cam = 0.0
@@ -117,9 +116,11 @@ def to_ccs(pt, center, pitch):
     xy = pitch * (pt - center)
     return np.append(xy, 0)
 
+
 def to_wcs(ijk_cam, xyz_u, t):
     """ccs to wcs (2.21)"""
     return np.dot(ijk_cam, xyz_u) + t
+
 
 def k_cam(phi, theta):
     """unit vector k_cam (2.22)"""
@@ -127,21 +128,26 @@ def k_cam(phi, theta):
                      np.sin(np.deg2rad(phi)),
                      -np.cos(np.deg2rad(phi)) * np.cos(np.deg2rad(theta))])
 
+
 def i_cam_0(j, k):
     """i_cam_0 (2.23)"""
     return np.cross(j, k) / LA.norm(np.cross(j, k))
+
 
 def j_cam_0(k, ic0):
     """j_cam_0 (2.24)"""
     return np.cross(k, ic0)
 
+
 def i_cam(ic0, jc0, kappa):
     """i_cam (2.25)"""
     return np.cos(np.deg2rad(kappa)) * ic0 + np.sin(np.deg2rad(kappa)) * jc0
 
+
 def j_cam(ic0, jc0, kappa):
     """j_cam (2.26)"""
     return -np.sin(np.deg2rad(kappa)) * ic0 + np.cos(np.deg2rad(kappa)) * jc0
+
 
 def r_listing(phi, theta):
     sp = np.sin(np.deg2rad(phi))
@@ -162,12 +168,14 @@ def r_listing(phi, theta):
         [m10, m11, m12],
         [m20, m21, m22]])
 
+
 def theta_eye_pp(alpha, beta):
     sa = np.sin(np.deg2rad(alpha))
     ca = np.cos(np.deg2rad(alpha))
     sb = np.sin(np.deg2rad(beta))
     cb = np.cos(np.deg2rad(beta))
     return -np.arctan((sa * cb) / (np.sqrt(ca**2 * cb**2 + sb**2)))
+
 
 def phi_eye_pp(alpha, beta):
     return -np.arctan(np.tan(np.deg2rad(beta)) / np.cos(np.deg2rad(alpha)))
@@ -179,9 +187,10 @@ def phi_eye_pp(alpha, beta):
 
 def b_norm(l1, l2, u1, u2, o):
     """intersection of planes (2.28)"""
-    b = np.cross(np.cross(l1 - o, u1 - o), \
+    b = np.cross(np.cross(l1 - o, u1 - o),
                  np.cross(l2 - o, u2 - o))
     return b / LA.norm(b)
+
 
 def curvaturecenter_c(kq, l, u, b, o, r):
     ou_n = (o - u) / LA.norm(o - u)
@@ -190,13 +199,16 @@ def curvaturecenter_c(kq, l, u, b, o, r):
     lq_n = (l - q) / LA.norm(l - q)
     return(q - r * ((lq_n + oq_n) / LA.norm(lq_n + oq_n)))
 
+
 def solve_kc_phd1(kc, l, u, b, o, r):
     ou_n = (o - u) / LA.norm(o - u)
-    kq = kc * np.dot(ou_n, b) - np.sqrt(kc**2 * np.dot(ou_n, b)**2 - kc**2 + r**2)
+    kq = (kc * np.dot(ou_n, b)
+          - np.sqrt(kc**2 * np.dot(ou_n, b)**2 - kc**2 + r**2))
     q = o + kq * ou_n
     oq_n = (o - q) / LA.norm(o - q)
     lq_n = (l - q) / LA.norm(l - q)
     return np.dot(lq_n - oq_n, q - o + kc * b)
+
 
 def solve_kc_phd2(kq, l1, l2, u1, u2, b, o, r):
     c1 = curvaturecenter_c(kq[0], l1, u1, b, o, r)
@@ -244,7 +256,9 @@ def calc_centers(glints, pupils, eye_R, eye_K):
 
     # calculate coordinate of pupil center
     ocov = np.dot(o - c, o - v)
-    kr = (-ocov - np.sqrt(ocov**2 - LA.norm(o - v)**2 * (LA.norm(o - c)**2 - eye_R**2))) / LA.norm(o - v)**2
+    kr = ((-ocov - np.sqrt(ocov**2 - LA.norm(o - v)**2
+                           * (LA.norm(o - c)**2 - eye_R**2)))
+          / LA.norm(o - v)**2)
     r = o + kr * (o - v)
 
     # |r - c| = R
@@ -258,7 +272,9 @@ def calc_centers(glints, pupils, eye_R, eye_K):
     np.testing.assert_almost_equal(eta0, eta1)
 
     eta = (eta0 + eta1) / 2
-    iota = n2/n1 * ((np.dot(nu, eta) - np.sqrt((n1/n2)**2 - 1 + np.dot(nu, eta)**2)) * nu - eta)
+    iota = n2/n1 * ((np.dot(nu, eta)
+                     - np.sqrt((n1/n2)**2 - 1 + np.dot(nu, eta)**2))
+                    * nu - eta)
     rci = np.dot(r - c, iota)
     kp = -rci - np.sqrt(rci**2 - (eye_R**2 - eye_K**2))
     p = r + kp * iota
@@ -270,11 +286,14 @@ def calc_centers(glints, pupils, eye_R, eye_K):
 
     return p, c
 
+
 def calc_gaze(eye_R, eye_K, eye_alpha, eye_beta):
     p, c = [], []
     for i in tqdm(range(len(ppos[0])), ncols=80):
-        pi, ci = calc_centers([glints[0][0][i], glints[0][1][i]], ppos[0][i], eye_R, eye_K)
-        # pi, ci = calc_centers([glints[1][0][i], glints[1][1][i]], ppos[1][i], eye_R, eye_K)
+        pi, ci = calc_centers([glints[0][0][i], glints[0][1][i]],
+                              ppos[0][i], eye_R, eye_K)
+        # pi, ci = calc_centers([glints[1][0][i], glints[1][1][i]],
+        #                       ppos[1][i], eye_R, eye_K)
         p.append(pi)
         c.append(ci)
     p = np.array(p)
@@ -321,6 +340,7 @@ def calc_gaze(eye_R, eye_K, eye_alpha, eye_beta):
 
     return intersect, c, w, w_rot
 
+
 def optimize_gaze(params):
     R, K, alpha, beta = params
     intersect, c, w, w_rot = calc_gaze(R, K, alpha, beta)
@@ -351,7 +371,8 @@ if __name__ == '__main__':
     ax.scatter(*intersect1.T, c='m', marker='.', linewidth=0.1)
     for i in range(0, len(c), 30):
         ax.plot(*np.array((c[i], c[i] + 400 * w[i])).T, c='b', linestyle='-')
-        ax.plot(*np.array((c[i], c[i] + 400 * w_rot[i])).T, c='g', linestyle='-')
+        ax.plot(*np.array((c[i], c[i] + 400 * w_rot[i])).T, c='g',
+                linestyle='-')
     for tgt in np.unique(targets, axis=0):
         ax.scatter(*tgt.T, c='k', marker='x')
     ax.auto_scale_xyz([-400, 400], [-400, 200], [500, 0])
