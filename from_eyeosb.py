3e9c67053b797e9ab2f24e82dbe683f90e82820b
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2018-06-13 18:09:03 +0200

    Add more debug output, plots

--- from_eyeosb.py
+++ from_eyeosb.py
@@ -12,21 +12,44 @@ def main(options):
     # create pandas dataframe from json recordings and do some preprocessing
     parser = EyeInfoParser(options)
     pp = Preprocessor(parser.get_dataframe(), options)
-    pp.rescale_to_screen('gaze_target')
-    pp.fix_reflex_positions()
-    pp.fliplr()
-    pp.flipud()
-
-    # final dataframe
+    pp.preprocess_all()
     df = pp.get_dataframe()
-    print(df.keys())
-    df.plot.scatter(x='left_eye.pupilpos.x', y='left_eye.pupilpos.y')
-    # df.plot.scatter(x='left_eye.gazepos.x', y='left_eye.gazepos.y')
-    # df.plot.scatter(x='right_eye.pupilpos.x', y='right_eye.pupilpos.y')
-    # df.plot.scatter(x='gaze_target.x', y='gaze_target.y')
+
+    for key in df.keys():
+        if all(ss not in key for ss in ["gaze"]):
+            print("{0: <30} {1}".format(key, df[key][0]))
+
+    fig, ax = plt.subplots(1, 1)
+
+    ax.scatter(df['left_eye.pupilpos.x'], df['left_eye.pupilpos.y'])
+    ax.scatter(df['left_eye.reflexpos.left.x'],
+               df['left_eye.reflexpos.left.y'], marker='.')
+    ax.scatter(df['left_eye.reflexpos.right.x'],
+               df['left_eye.reflexpos.right.y'], marker='.')
+    # ax.scatter(df['left_eye.reflex_center.x'],
+    #            df['left_eye.reflex_center.y'], marker='.')
+    ax.scatter(df['right_eye.pupilpos.x'], df['right_eye.pupilpos.y'])
+    ax.scatter(df['right_eye.reflexpos.left.x'],
+               df['right_eye.reflexpos.left.y'], marker='.')
+    ax.scatter(df['right_eye.reflexpos.right.x'],
+               df['right_eye.reflexpos.right.y'], marker='.')
+    # ax.scatter(df['right_eye.reflex_center.x'],
+    #            df['right_eye.reflex_center.y'], marker='.')
+    ax.set_xlim([0, options.cam_x * options.cam_pp])
+    ax.set_ylim([options.cam_y * options.cam_pp, 0])
     plt.show()
 
-    # mapper = GazeMapper(options)
+    # ax.scatter(df['gaze_target.x'], df['gaze_target.y'])
+    # ax.scatter(df['gaze_point.x'], df['gaze_point.y'], marker='x')
+    # ax.scatter(df['left_eye.gazepos.x'],
+    #            df['left_eye.gazepos.y'], marker='.')
+    # ax.scatter(df['right_eye.gazepos.x'],
+    #            df['right_eye.gazepos.y'], marker='.')
+    # ax.set_xlim([0, options.screen_x * options.screen_pp])
+    # ax.set_ylim([options.screen_y * options.screen_pp, 0])
+    # plt.show()
+
+    mapper = GazeMapper(options, df)
 
 
 if __name__ == '__main__':
@@ -36,8 +59,15 @@ if __name__ == '__main__':
     p.add('--recording', type=str, help='Input recording (json)')
     p.add('--output_dataframe', type=str,
           help='Output file for parsed recording')
+    p.add('--n1', type=float, help='Refractive index n1')
+    p.add('--n2', type=float, help='Refractive index n2')
+    p.add('--cam_pp', type=float, help='Camera pixel pitch')
+    p.add('--phi_cam', type=float, help='Camera phi')
+    p.add('--theta_cam', type=float, help='Camera theta')
+    p.add('--kappa_cam', type=float, help='Camera kappa')
     p.add('--screen_x', type=int, help='Screen x resolution')
     p.add('--screen_y', type=int, help='Screen y resolution')
+    p.add('--screen_pp', type=float, help='Screen pixel pitch')
     p.add('--cam_x', type=int, help='Screen x resolution')
     p.add('--cam_y', type=int, help='Screen y resolution')
     options = p.parse_args()
