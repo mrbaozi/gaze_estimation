2a3162539d3eca2245ee324ec49ed895e321420d
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2018-10-31 16:42:09 +0100

    Add sterep calibration function

--- resources/calibration/chessboard_calibration.py
+++ resources/calibration/chessboard_calibration.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os
 import argparse
-import glob
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def calibrate(folder, square_size=25, save_output=False):
+def mono_calibrate(folder, square_size=25, save_output=False):
     term_crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((6*9, 3), np.float32)
@@ -33,6 +31,8 @@ def calibrate(folder, square_size=25, save_output=False):
             imgpoints.append(corners)
 
     objpoints = np.array(objpoints)
+    imgpoints = np.array(imgpoints)
+
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
                                                        gray.shape[::-1],
                                                        None, None,
@@ -62,10 +62,59 @@ def calibrate(folder, square_size=25, save_output=False):
     return [ret, mtx, dist, rvecs, tvecs]
 
 
+def stereo_calibrate(args, cam1_calib, cam2_calib, square_size=25):
+    term_crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    objp = np.zeros((1, 6*9, 3), np.float32)
+    objp[0, :, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2) * square_size
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    # image list for plotting (with chessboard corners)
+    images = []
+
+    for image in [args.cam1, args.cam2]:
+        img = cv2.imread(image[0])
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # this is specific to the image - we have two chessboards in the image,
+        # and for this we need to use the left one
+        if image == args.cam2:
+            gray[:, 1000:] = 0
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), term_crit)
+            imgpoints.append([corners])
+            cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+            images.append(img)
+
+    objpoints = np.array(objpoints)
+    imgpoints = np.array(imgpoints)
+
+    _, _, _, _, _, R, T, E, F = cv2.stereoCalibrate(
+        objpoints[0], imgpoints[0], imgpoints[1],
+        cam1_calib[1], cam1_calib[2],
+        cam2_calib[1], cam2_calib[2],
+        gray.shape[::-1], flags=cv2.CALIB_FIX_INTRINSIC, criteria=term_crit)
+
+    print('Rotation matrix:\n{}'.format(R))
+    print('Translation matrix:\n{}'.format(T))
+    print('Essential matrix:\n{}'.format(E))
+    print('Fundamental matrix:\n{}'.format(F))
+
+
 def main(args):
     calibrations = []
     for folder in args.folders:
-        calibrations.append(calibrate(folder, 25, args.save))
+        calibrations.append(mono_calibrate(folder, 25, args.save))
+
+    stereo_calibrate(args, calibrations[0], calibrations[1])
 
 
 if __name__ == '__main__':
@@ -74,5 +123,9 @@ if __name__ == '__main__':
                         help='List of folders with calibration images')
     parser.add_argument('--save', '-s', action='store_true',
                         help='Save calibrations to file')
+    parser.add_argument('--cam1', type=str, nargs=1, required=True,
+                        help='Calibration image of camera 1')
+    parser.add_argument('--cam2', type=str, nargs=1, required=True,
+                        help='Calibration image of camera 2')
     args = parser.parse_args()
     main(args)
