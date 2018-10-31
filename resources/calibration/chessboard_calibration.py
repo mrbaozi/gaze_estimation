d4f242e4296b6e7128996ff341f23d778fe70a05
Yannick Augenstein <yannick.augenstein@iosb.fraunhofer.de>
2018-10-31 14:19:09 +0100

    Rewrite calibration routine

--- resources/calibration/chessboard_calibration.py
+++ resources/calibration/chessboard_calibration.py
@@ -1,56 +1,78 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import cv2
+import os, sys
+import argparse
 import glob
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9, 3), np.float32) * 25
-objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+def calibrate(folder, square_size=25, save_output=False):
+    term_crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2) * square_size
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
 
-images = glob.glob('images/*.tiff')
+    for fname in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, fname))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), term_crit)
+            imgpoints.append(corners)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+    objpoints = np.array(objpoints)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
+                                                       gray.shape[::-1],
+                                                       None, None,
+                                                       criteria=term_crit)
+    fovx, fovy, focalLength, principalPoint, aspectRatio = \
+        cv2.calibrationMatrixValues(mtx, gray.shape[::-1], 5.2, 3.88)
 
-        cv2.cornerSubPix(gray,corners,(11, 11),(-1, -1), criteria)
-        imgpoints.append(corners)
+    if save_output:
+        with open(os.path.basename(os.path.normpath(folder))
+                  + '_calibration.txt', 'w') as ofile:
+            ofile.write("Average reprojection error: {}\n".format(ret))
+            ofile.write("Camera intrinsic matrix:\n{}\n".format(mtx))
+            ofile.write("Distortion coefficients:\n{}\n".format(dist))
+            ofile.write("Per image rotation vectors:\n{}\n".format(rvecs))
+            ofile.write("Per image translation vectors:\n{}\n"
+                        .format(tvecs))
+            ofile.write("Field of view in degress along "
+                        "horizontal sensor axis:\n{}\n".format(fovx))
+            ofile.write("Field of view in degress along "
+                        "vertical sensor axis:\n{}\n".format(fovy))
+            ofile.write("Focal length of lens in mm:\n{}\n"
+                        .format(focalLength))
+            ofile.write("Principal point in mm:\n{}\n"
+                        .format(principalPoint))
+            ofile.write("Pixel aspect ratio:\n{}".format(aspectRatio))
 
-        # # Draw and display the corners
-        # cv2.drawChessboardCorners(img, (9, 6), corners,ret)
-        # cv2.imshow('img', img)
-        # cv2.waitKey(500)
+    return [ret, mtx, dist, rvecs, tvecs]
 
-cv2.destroyAllWindows()
 
-objpoints = np.array(objpoints) * 28
+def main(args):
+    calibrations = []
+    for folder in args.folders:
+        calibrations.append(calibrate(folder, 25, args.save))
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None, criteria=criteria)
-fovx, fovy, focalLength, principalPoint, aspectRatio = cv2.calibrationMatrixValues(mtx, gray.shape[::-1], 5.2, 3.88)
 
-print("Average reprojection error: {}".format(ret))
-print("Camera intrinsic matrix:\n{}".format(mtx))
-print("Distortion coefficients:\n{}".format(dist))
-print("Per image rotation vectors:\n{}".format(rvecs))
-print("Per image translation vectors:\n{}".format(tvecs))
-print("Field of view in degress along horizontal sensor axis:\n{}".format(fovx))
-print("Field of view in degress along vertical sensor axis:\n{}".format(fovy))
-print("Focal length of lens in mm:\n{}".format(focalLength))
-print("Principal point in mm:\n{}".format(principalPoint))
-print("Pixel aspect ratio:\n{}".format(aspectRatio))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folders', '-f', type=str, nargs='+', required=True,
+                        help='List of folders with calibration images')
+    parser.add_argument('--save', '-s', action='store_true',
+                        help='Save calibrations to file')
+    args = parser.parse_args()
+    main(args)
